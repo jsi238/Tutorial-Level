884b74c4803c6b73fd24527e89262d12699c6864
Jordan Imp <49874843+jsi238@users.noreply.github.com>
2024-10-04 21:39:26 -0400

    Added comments and attempted to fix some collisions
    Movement works pretty well for the most part. Still some weirdness when colliders are too close to one another.

--- My project/Assets/Scripts/Movement.cs
+++ My project/Assets/Scripts/Movement.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEditor.UI;
 using UnityEditorInternal;
 using UnityEngine;
@@ -7,13 +8,14 @@ using UnityEngine.UIElements.Experimental;
 
 public class Movement : MonoBehaviour
 {
-    [SerializeField] float playerSpeed = 1f;
+    [SerializeField] float playerSpeed = 1f; //create variable to set player speed
 
-    private GameObject rock;
+    private GameObject rock; //rock object to be removed when crushed
 
-    private bool isTouchingRock = false;
+    private bool isTouchingRock = false; //check if player is a touching rock
 
-    private bool isTouchingRight = false;
+    /*check which direction their is an obstacle to stop movement in that direction */
+    private bool isTouchingRight = false; 
     private bool isTouchingLeft = false;
     private bool isTouchingUp = false;
     private bool isTouchingDown = false;
@@ -27,6 +29,7 @@ public class Movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        /*maybe less typing to just do it this way */
         bool moveUp = Input.GetKey(KeyCode.W);
         bool moveDown = Input.GetKey(KeyCode.S);
         bool moveLeft = Input.GetKey(KeyCode.A);
@@ -34,6 +37,7 @@ public class Movement : MonoBehaviour
 
         bool destroyRock = Input.GetKeyDown(KeyCode.F);
 
+        /* check which key is pressed and if movement is impeded in that directions */
         if (moveUp && isTouchingUp == false)
         {
             this.gameObject.transform.Translate(Vector3.up * playerSpeed * Time.deltaTime);
@@ -52,48 +56,58 @@ public class Movement : MonoBehaviour
         }
         if (destroyRock && isTouchingRock)
         {
-            rock.SetActive(false);
+            rock.SetActive(false); //'destory' rock in players way
         }
     }
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.tag == "pig" || collision.tag == "horse" || collision.tag == "cow")
+        Debug.Log("HeRE come dAt boi!");
+
+        if (collision.gameObject.tag == "pig" || collision.gameObject.tag == "horse" || collision.gameObject.tag == "cow")
         {
-            collision.gameObject.SetActive(false);
+            collision.gameObject.SetActive(false); //collect animals when touched
         }
+
+        if (collision.gameObject.tag == "rock" || collision.gameObject.tag == "fence") //detects player is touching a rock or fence
+        {
+            rock = collision.gameObject;
+            Vector2 direction = collision.GetContact(0).normal;
+            /* determine if playing is making contact from above, below, left, or right */
+            if (direction.x == 1)
+                isTouchingLeft = true;
+            if (direction.x == -1)
+                isTouchingRight = true;
+            if (direction.y == 1)
+                isTouchingDown = true;
+            if (direction.y == -1)
+                isTouchingUp = true;
+        }
+
     }
 
-    private void OnTriggerStay2D(Collider2D collision)
+    private void OnCollisionStay2D(Collision2D collision)
     {
         Vector2 collisionPos = new Vector2(collision.gameObject.transform.position.x, collision.gameObject.transform.position.y);
         Vector2 playerPos = this.transform.position;
 
-        if (collision.tag == "rock")
+        if (collision.gameObject.tag == "rock")
         {
-            isTouchingRock = true;
-            rock = collision.gameObject;
-            if (collisionPos.x > playerPos.x)
-            {
-                isTouchingRight = true;
-            }
-            if (collisionPos.x < playerPos.x)
-            {
-                isTouchingLeft = true;
-            }
-            
+            isTouchingRock = true; //check if player is actively touching rock      
         }
     }
 
-    private void OnTriggerExit2D(Collider2D collision)
+    private void OnCollisionExit2D(Collision2D collision)
     {
-        if (collision.tag == "rock")
+        if (collision.gameObject.tag == "rock")
         {
-            isTouchingRock = false;
-            isTouchingLeft = false;
-            isTouchingRight = false;
-            isTouchingDown = false;
-            isTouchingUp = false;
+            isTouchingRock = false; //check that player is no longer touching rock
         }
+
+        /* reset is touching since we are no longer touching */
+        isTouchingLeft = false;
+        isTouchingRight = false;
+        isTouchingDown = false;
+        isTouchingUp = false;
     }
 }
