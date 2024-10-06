04ef885ecff09a23c34e7e7a5553f0972f51f7df
awu0 <82164146+awu0@users.noreply.github.com>
2024-10-06 13:27:23 -0400

    fixed collision

--- My project/Assets/Scripts/Movement.cs
+++ My project/Assets/Scripts/Movement.cs
@@ -12,19 +12,11 @@ public class Movement : MonoBehaviour
     [SerializeField] float playerSpeed = 1f; //create variable to set player speed
     private Vector2 movement; // vector for movement
     private Rigidbody2D rb;
-    private bool idle = true;
     private bool facingRight = true;
 
     private GameObject rock; //rock object to be removed when crushed
 
     private bool isTouchingRock = false; //check if player is a touching rock
-
-    /*check which direction their is an obstacle to stop movement in that direction */
-    private bool isTouchingRight = false;
-    private bool isTouchingLeft = false;
-    private bool isTouchingUp = false;
-    private bool isTouchingDown = false;
-
 
     // Start is called before the first frame update
     void Start()
@@ -34,7 +26,6 @@ public class Movement : MonoBehaviour
         animator.SetBool("Idle", true);
     }
 
-    // Update is called once per frame
     void Update()
     {
         /*maybe less typing to just do it this way */
@@ -46,13 +37,13 @@ public class Movement : MonoBehaviour
         bool destroyRock = Input.GetKeyDown(KeyCode.F);
 
         /* check which key is pressed and if movement is impeded in that directions */
-        if (moveUp && isTouchingUp == false) movement.y = 1;
-        if (moveDown && isTouchingDown == false) movement.y = -1;
-        if (moveLeft && isTouchingLeft == false) {
+        if (moveUp) movement.y = 1;
+        if (moveDown ) movement.y = -1;
+        if (moveLeft ) {
             movement.x = -1;
             facingRight = false;
         }
-        if (moveRight && isTouchingRight == false) {
+        if (moveRight ) {
             movement.x = 1;
             facingRight = true;
         }
@@ -77,10 +68,8 @@ public class Movement : MonoBehaviour
         bool moving = moveRight || moveLeft || moveUp || moveDown;
 
         if (moving) {
-            idle = false;
             animator.SetBool("isWalking", true);
         } else {
-            idle = true;
             animator.SetBool("isWalking", false);
         }
 
@@ -103,40 +92,20 @@ public class Movement : MonoBehaviour
 
         if (collision.gameObject.tag == "rock") //detects player is touching a rock or fence
         {
+            rb.velocity = Vector2.zero;
             rock = collision.gameObject;
         }
-        Vector2 direction = collision.GetContact(0).normal;
-        /* determine if playing is making contact from above, below, left, or right */
-        if (direction.x == 1)
-            isTouchingLeft = true;
-        if (direction.x == -1)
-            isTouchingRight = true;
-        if (direction.y == 1)
-            isTouchingDown = true;
-        if (direction.y == -1)
-            isTouchingUp = true;
     }
 
     private void OnCollisionStay2D(Collision2D collision)
     {
-        Vector2 collisionPos = new Vector2(collision.gameObject.transform.position.x, collision.gameObject.transform.position.y);
-        Vector2 playerPos = this.transform.position;
+        // Vector2 collisionPos = new Vector2(collision.gameObject.transform.position.x, collision.gameObject.transform.position.y);
+        // Vector2 playerPos = this.transform.position;
 
         if (collision.gameObject.tag == "rock")
         {
             isTouchingRock = true; //check if player is actively touching rock
         }
-
-        Vector2 direction = collision.GetContact(0).normal;
-        /* determine if playing is making contact from above, below, left, or right */
-        if (direction.x == 1)
-            isTouchingLeft = true;
-        if (direction.x == -1)
-            isTouchingRight = true;
-        if (direction.y == 1)
-            isTouchingDown = true;
-        if (direction.y == -1)
-            isTouchingUp = true;
     }
 
     private void OnCollisionExit2D(Collision2D collision)
@@ -145,11 +114,5 @@ public class Movement : MonoBehaviour
         {
             isTouchingRock = false; //check that player is no longer touching rock
         }
-
-        /* reset is touching since we are no longer touching */
-        isTouchingLeft = false;
-        isTouchingRight = false;
-        isTouchingDown = false;
-        isTouchingUp = false;
     }
 }
